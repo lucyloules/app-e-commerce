91717c2bb3a9aaede456a7485e6b17d727137823
Claudina Cortez de Leon <claudinacr@gmail.com>
2018-02-24 17:44:11 -0300

    Lista de artículos

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,7 +25,6 @@ fetch('https://api.mercadolibre.com/sites/MLC/search?condition=all&q=all', {
     .then(function (product) {
         console.log(product);
         let listCateg = product;
-
         let butt = document.querySelector('.butt');
         let inputSearch = document.querySelector('.inputSearch');
 
@@ -36,7 +35,49 @@ fetch('https://api.mercadolibre.com/sites/MLC/search?condition=all&q=all', {
                 let valuesResults = (listCateg.results[i].title);
                 let regex = new RegExp(valueFlip, 'i', 'g');
                 if (regex.test(valuesResults) === true) {
-                    console.log(valuesResults);
+                    console.log(listCateg.results[i].title);
+                    console.log(listCateg.results[i].price);
+                    console.log(listCateg.results[i].available_quantity);
+                    console.log(listCateg.results[i].thumbnail);
+
+                    let resultsSearchInput = document.querySelector('.resultsSearchInput');
+                    let divResultsInput = document.createElement('div'); 
+                    divResultsInput.className = 'divResultsInput';
+                    let divImages = document.createElement('div'); 
+                    divImages.className='divImages';
+                    let image = document.createElement('img');
+                    image.className = 'image';
+                    image.setAttribute('src', listCateg.results[i].thumbnail);
+                    divImages.appendChild(image);
+
+                    let titleProduc = document.createTextNode(listCateg.results[i].title);
+                    let pTitle = document.createElement('p');
+                    pTitle.appendChild(titleProduc);
+                    divTitle = document.createElement('div');
+                    divTitle.appendChild(pTitle);
+                    divTitle.className='divTitle';
+
+
+                    let priceProduc = document.createTextNode(listCateg.results[i].price);
+                    let pPrice = document.createElement('p');
+                    pPrice.appendChild(priceProduc);
+                    divPrice = document.createElement('div');
+                    divPrice.appendChild(pPrice);
+
+                    let pAvailable = document.createElement('p');
+                    let availableProduc = document.createTextNode(listCateg.results[i].available_quantity);
+                    pAvailable.appendChild(availableProduc);
+                    divAvailable = document.createElement('div');
+                    divAvailable.appendChild(pAvailable);
+
+
+                    divResultsInput.appendChild(divImages);
+                    divResultsInput.appendChild(divTitle);
+                    divResultsInput.appendChild(divPrice);
+                    divResultsInput.appendChild(divAvailable);
+                    resultsSearchInput.appendChild(divResultsInput);
+
+
                 }
             }
             inputSearch.value = '';
@@ -81,7 +122,7 @@ let categoriesSelect = document.querySelector('.categoriesSelect');
 categoriesSelect.addEventListener('change', function () {
     let idCategoria = categoriesSelect.value;
 
-    fetch('https://api.mercadolibre.com/sites/MLC/search?category='+idCategoria, {
+    fetch('https://api.mercadolibre.com/sites/MLC/search?category=' + idCategoria, {
             method: 'GET',
             mode: 'cors',
             headers: {
@@ -94,9 +135,51 @@ categoriesSelect.addEventListener('change', function () {
         })
         .then(function (info) {
             console.log(info);
-            
+
             for (let k = 0; k < info.results.length; k++) {
+                console.log(info.results[k].price);
                 console.log(info.results[k].title);
+                console.log(info.results[k].available_quantity);
+                console.log(info.results[k].thumbnail);
+
+                let resultsList = document.querySelector('.resultsList');
+                    let divResultsInput = document.createElement('div'); 
+                    divResultsInput.className = 'divResultsInput';
+                    let divImages = document.createElement('div'); 
+                    divImages.className='divImages';
+                    let image = document.createElement('img');
+                    image.className = 'image';
+                    image.setAttribute('src', info.results[k].thumbnail);
+                    divImages.appendChild(image);
+
+                    let titleProduc = document.createTextNode(info.results[k].title);
+                    let pTitle = document.createElement('p');
+                    pTitle.appendChild(titleProduc);
+                    divTitle = document.createElement('div');
+                    divTitle.appendChild(pTitle);
+                    divTitle.className='divTitle';
+
+
+                    let priceProduc = document.createTextNode(info.results[k].price);
+                    let pPrice = document.createElement('p');
+                    pPrice.appendChild(priceProduc);
+                    divPrice = document.createElement('div');
+                    divPrice.appendChild(pPrice);
+
+                    let pAvailable = document.createElement('p');
+                    let availableProduc = document.createTextNode(info.results[k].available_quantity);
+                    pAvailable.appendChild(availableProduc);
+                    divAvailable = document.createElement('div');
+                    divAvailable.appendChild(pAvailable);
+
+
+                    divResultsInput.appendChild(divImages);
+                    divResultsInput.appendChild(divTitle);
+                    divResultsInput.appendChild(divPrice);
+                    divResultsInput.appendChild(divAvailable);
+                    resultsList.appendChild(divResultsInput);
+
+
             }
         })
 })
