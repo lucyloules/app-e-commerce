7391e7d176b3e568b1d2dcb01cc68205094ca2ed
Lucyloules <lucia.lagos@gmail.com>
2018-02-25 12:19:09 -0300

    autentificación Firebase (ingreso y registro)

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,38 +1,91 @@
+/* Login Firebase */
+// Registro de nuevos usuarios*/
+function registrar() {
+  console.log('diste click en Ingresar');
+  let email2 = document.getElementById('email2').value;
+  let password2 = document.getElementById('pwd2').value;
+  console.log(email2);
+  console.log(pwd2);
+  firebase.auth().createUserWithEmailAndPassword(email2, pwd2)
+    .then(function() {
+      // verificarEmail()
+    })
+    .catch(function(error) { // promesa catch, si la autentificacion no ocurre catch ejecuta funcion con parametro e, donde se guardo 2 errores en variables
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // console.log(errorCode);
+      // console.log(errorMessage);
+      alert(error.code);
+    });
+};
+
+// Ingreso usuarios o logueo
+function ingresar() {
+  console.log('diste click en Ingresar');
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  console.log(email);
+  console.log(password);
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // console.log(errorCode);
+      // console.log(errorMessage);
+      alert(error.code);
+    });
+};
+
+// Funcion para desloguearse Pendiente de terminar
+function cerrar() {
+  firebase.auth().signOut() // Cierra sesion desde firebase, toma 2 parametros then y catch
+    .then(function() { // (respuesta positiva)
+      // Sign-out successful.
+      console.log('Sesión cerrada');
+    }).catch(function(error) {// (respuesta negativa)/error : parametro
+      // An error happened.
+      console.log(error);
+    });
+}
+
+/* --------------------------------------------------------------------------------------------------------------- */
 /* Inicializando Cloud Firestore */
 firebase.initializeApp({
-  apiKey: "AIzaSyAmkjOzP-gCKgvNm0kEJCYWd56GcGncmss",
-  authDomain: "ecommerce-7b26c.firebaseapp.com",
-  projectId: "ecommerce-7b26c"
+  apiKey: 'AIzaSyAmkjOzP-gCKgvNm0kEJCYWd56GcGncmss',
+  authDomain: 'ecommerce-7b26c.firebaseapp.com',
+  projectId: 'ecommerce-7b26c'
 });
 
 // Initialize Cloud Firestore through Firebase
 var db = firebase.firestore();
 
-//Agregar documentos
+// Agregar documentos
 function guardar() {
   var nombre = document.getElementById('nombre').value;
   var apellido = document.getElementById('apellido').value;
   var fecha = document.getElementById('fecha').value;
 
-  db.collection("users").add({
+  db.collection('users').add({
     first: nombre,
     last: apellido,
     born: fecha
   })
-    .then(function (docRef) {
-      console.log("Document written with ID: ", docRef.id);
+    .then(function(docRef) {
+      console.log('Document written with ID: ', docRef.id);
       document.getElementById('nombre').value = '';
       document.getElementById('apellido').value = '';
       document.getElementById('fecha').value = '';
     })
-    .catch(function (error) {
-      console.error("Error adding document: ", error);
+    .catch(function(error) {
+      console.error('Error adding document: ', error);
     });
 }
 
-//Leer documentos
+// Leer documentos
 var tabla = document.getElementById('tabla');
-db.collection("users").onSnapshot((querySnapshot) => {
+db.collection('users').onSnapshot((querySnapshot) => {
   tabla.innerHTML = '';
   querySnapshot.forEach((doc) => {
     console.log(`${doc.id} => ${doc.data().first}`);
@@ -45,30 +98,29 @@ db.collection("users").onSnapshot((querySnapshot) => {
         <td><button class="btn btn-danger" onclick="eliminar('${doc.id}')">Eliminar</button></td>
         <td><button class="btn btn-warning" onclick="editar('${doc.id}','${doc.data().first}','${doc.data().last}','${doc.data().born}')">Editar</button></td>
         </tr>
-        `
+        `;
   });
 });
 
-//borrar documentos
+// borrar documentos
 function eliminar(id) {
-  db.collection("users").doc(id).delete().then(function () {
-    console.log("Document successfully deleted!");
-  }).catch(function (error) {
-    console.error("Error removing document: ", error);
+  db.collection('users').doc(id).delete().then(function() {
+    console.log('Document successfully deleted!');
+  }).catch(function(error) {
+    console.error('Error removing document: ', error);
   });
 }
 
-//editar documentos
+// editar documentos
 function editar(id, nombre, apellido, fecha) {
-
   document.getElementById('nombre').value = nombre;
   document.getElementById('apellido').value = apellido;
   document.getElementById('fecha').value = fecha;
   var boton = document.getElementById('boton');
   boton.innerHTML = 'Editar';
 
-  boton.onclick = function () {
-    var usersRef = db.collection("users").doc(id);
+  boton.onclick = function() {
+    var usersRef = db.collection('users').doc(id);
     var nombre = document.getElementById('nombre').value;
     var apellido = document.getElementById('apellido').value;
     var fecha = document.getElementById('fecha').value;
@@ -78,16 +130,16 @@ function editar(id, nombre, apellido, fecha) {
       last: apellido,
       born: fecha
     })
-      .then(function () {
-        console.log("Document successfully updated!");
+      .then(function() {
+        console.log('Document successfully updated!');
         boton.innerHTML = 'Guardar';
         document.getElementById('nombre').value = '';
         document.getElementById('apellido').value = '';
         document.getElementById('fecha').value = '';
       })
-      .catch(function (error) {
+      .catch(function(error) {
         // The document probably doesn't exist.
-        console.error("Error actualizando el archivo: ", error);
+        console.error('Error actualizando el archivo: ', error);
       });
-  }
+  };
 }
